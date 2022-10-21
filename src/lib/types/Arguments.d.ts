b274c12d3239c12e500a6b051e6eff3d8843049b
ChikoShidori <degnan200@gmail.com>
2022-10-21 18:02:35 +0000

    chore: useless shit

--- src/lib/types/Arguments.d.ts
+++ src/lib/types/Arguments.d.ts
@@ -1,15 +1,8 @@
-import type { PrismaClient } from '@prisma/client';
 import type { ArrayString } from '@skyra/env-utilities';
 
 declare module 'discord.js' {
 	interface Client {
 		dev: boolean;
-	}
-}
-
-declare module '@sapphire/pieces' {
-	interface Container {
-		prisma: PrismaClient;
 	}
 }
 
@@ -23,7 +16,6 @@ declare module '@skyra/env-utilities' {
 	interface Env {
 		CLIENT_OWNERS: ArrayString;
 
-		DATABASE_URL: string;
 		DISCORD_TOKEN: string;
 	}
 }
